a988f9c4ae060fd8d4d96d32752dcdcfa3d89b94
Ivan Antipenko <johnnAnt@yandex.ru>
2023-05-26 01:11:56 +0300

    test

--- src/routes/blogRouter.ts
+++ src/routes/blogRouter.ts
@@ -9,7 +9,7 @@ import { Router } from 'express';
 const blogRouter = Router();
 export default blogRouter;
 
-blogRouter.get('/', getPosts);
+blogRouter.post('/get', getPosts);
 blogRouter.post('/', createPost);
 blogRouter.delete('/:id', deletePost);
 blogRouter.patch('/:id', changePost);
